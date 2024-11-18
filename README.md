9cf01c72c892b74d3dde4046491c1ca06d4f7898
Peter McGoron <code@mcgoron.com>
2024-11-18 18:19:10 -0500

    brainfuck->scheme

---
+++ README.md
@@ -0,0 +1,46 @@
+# Brainfuck2Scheme
+
+A simple compiler from Brainfuck to R5RS. The compiler will output a Scheme
+list that is a lambda with two arguments:
+
+* `data`: The data vector.
+* `dptr`: The initial data pointer.
+
+To turn the list into an executable Scheme function, just give it to
+`eval`.
+
+## How It Works
+
+Brainfuck is a very simple Harvard architecture computer. The data is
+stored as a vector `data` and the data pointer is an integer `dptr`.
+The big idea is that all the code is compiled to a big lambda form, but
+there are some wrinkles due to jumps.
+
+Data access brainfuck instructions are translated like
+
+* `+` -> `(vector-set! data dptr (+ 1 (vector-ref data dptr)))`
+* `-` -> `(vector-set! data dptr (+ -1 (vector-ref data dptr)))`
+* `>` -> `(set! dptr (+ dptr 1))`
+* `<` -> `(set! dptr (- dptr 1))`
+* `.` -> `(display (vector-ref data dptr))`
+* `,` -> `(vector-set! data dptr (read-char))`
+
+Branches are trickier. Basically, all code that will be executed in a block
+is in a lambda. Given `[code...]`, the `code...` will be compiled to a
+lambda in a `letrec`, with a conditional at the end that will tail-call the
+lambda if the current data pointer is not zero.
+
+The transformation then goes like
+
+`[ CODE ... ] REST ...]` ->
+
+    (letrec ((between (lambda ()
+                        (TRANSLATE CODE ...)
+                        (if (not (zero? vector-ref data dptr)
+                            (between))))))
+      (if (not (zero? (vector-ref data dptr)))
+          (between)))
+    (TRANSLATE REST ...)
+
+where `(TRANSLATE CODE ...)` translates `CODE` to Scheme instructions
+like above.
---
+++ bf2s.scm
@@ -0,0 +1,78 @@
+;;; Brainfuck->Scheme compiler.
+;;; 
+;;; Copyright 2024 Peter McGoron
+;;; 
+;;; Licensed under the Apache License, Version 2.0 (the "License");
+;;; you may not use this file except in compliance with the License.
+;;; You may obtain a copy of the License at
+;;; 
+;;;     http://www.apache.org/licenses/LICENSE-2.0
+;;; 
+;;; Unless required by applicable law or agreed to in writing, software
+;;; distributed under the License is distributed on an "AS IS" BASIS,
+;;; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+;;; See the License for the specific language governing permissions and
+;;; limitations under the License.
+
+;;; Turn a reversed list of Scheme commands into a thunk.
+(define (assemble-function ins)
+  `(lambda ()
+     ,@(reverse ins)))
+
+;;; Compile all brainfuck instructions in LST, coming after the Scheme
+;;; commands in INS.
+(define (compile lst ins)
+  (if (null? lst)
+      (assemble-function ins)
+      (case (car lst)
+        ((#\>) (compile (cdr lst)
+                        (cons '(set! dptr (+ dptr 1)) ins)))
+        ((#\<) (compile (cdr lst)
+                        (cons '(set! dptr (- dptr 1)) ins)))
+        ((#\+) (compile (cdr lst)
+                        (cons
+                         '(vector-set! data dptr
+                                       (+ (vector-ref data dptr) 1))
+                         ins)))
+        ((#\-) (compile (cdr lst)
+                        (cons
+                         '(vector-set! data dptr
+                                       (- (vector-ref data dptr) 1))
+                         ins)))
+        ((#\.) (compile (cdr lst)
+                        (cons '(display (integer->char (vector-ref data dptr))) ins)))
+        ((#\,) (compile (cdr lst)
+                        (cons '(vector-set! data dptr
+                                            (char->integer (read-char))) ins)))
+        ((#\[) (let ((rest (compile (cdr lst) '())))
+                 (if (not (pair? rest))
+                     (error "unmatched [")
+                     (let ((between (car rest))
+                           (after-uncompiled (cdr rest)))
+                       (compile after-uncompiled
+                                (cons `(letrec ((between ,between))
+                                         (if (not (zero? (vector-ref data dptr)))
+                                             (between)))
+                                      ins))))))
+        ((#\]) (cons (assemble-function
+                      (cons '(if (not (zero? (vector-ref data dptr)))
+                                 (between))
+                            ins))
+                     (cdr lst)))
+        (else (compile (cdr lst) ins)))))
+
+(define (brainfuck->scheme str)
+  `(lambda (data dptr)
+     (,(compile (string->list str) '()))))
+
+(define (brainfuck->scheme-from-file filename)
+  (brainfuck->scheme
+   (call-with-port (open-input-file filename)
+     (lambda (port)
+       (let loop ((str ""))
+         (if (eof-object? (peek-char port))
+             str
+             (loop (string-append str (read-line port)))))))))
+
+(define (execute scheme)
+  ((eval scheme) (make-vector 4096) 0))
